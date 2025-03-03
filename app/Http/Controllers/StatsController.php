cb280f60ba2108a1a0d288e97c33ec4e7b058cd5
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2025-03-04 02:36:05 +0530

    udpated

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -12,12 +12,14 @@ class StatsController
      */
     public function index()
     {
-        $lastMonthDataList = Stats::where('created_at', '>=', now()->startOfMonth()->subMonth())->where('created_at', '<=', now()->endOfMonth()->subMonth())->get();
-        $thisMonthDataList = Stats::where('created_at', '>=', now()->startOfMonth())->where('created_at', '<=', now()->endOfMonth())->get();
+        $lastMonthDataList = Stats::whereBetween('created_at', [now()->startOfMonth()->subMonth(), now()->endOfMonth()->subMonth()])->get();
+        $thisMonthDataList = Stats::whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()])->get();
+
         return response()->json([
             'last_month' => $lastMonthDataList,
             'this_month' => $thisMonthDataList
         ]);
+
     }
 
     /**
