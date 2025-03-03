7d7e5c438548395a11ab6fe7ee8ffb7859e9221d
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2025-03-04 02:42:42 +0530

    test

--- database/seeders/StatsSeeder.php
+++ database/seeders/StatsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stats;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class StatsSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class StatsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            Stats::create([
+                'created_at' => Carbon::now()->subMonth()->startOfMonth()->addDays(rand(0, 29))
+                    ->setTime(rand(0, 23), rand(0, 59), rand(0, 59)),
+                'updated_at' => Carbon::now()->subMonth()->startOfMonth()->addDays(rand(0, 29))
+                    ->setTime(rand(0, 23), rand(0, 59), rand(0, 59)),
+            ]);
+        }
     }
 }
