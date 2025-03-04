624336324794e7efe6ad5c019d518b947c957326
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2025-03-04 06:44:09 +0530

    seed

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Order::create([
+            'orderItems' => json_encode([
+                [
+                    'name' => 'Burger',
+                    'price' => 10.99,
+                    'quantity' => 2,
+                ],
+                [
+                    'name' => 'Fries',
+                    'price' => 5.99,
+                    'quantity' => 1,
+                ],
+            ]),
+            'userUID' => '12hg123gh1g31231231',
+            'totalPrice' => 27.97,
+            'status' => 'Pending',
+        ]);
+
+        Order::create([
+            'orderItems' => json_encode([
+                [
+                    'name' => 'Burger',
+                    'price' => 15.99,
+                    'quantity' => 1,
+                ],
+            ]),
+            'userUID' => '12hg123gh1g31231231',
+            'totalPrice' => 57.67,
+            'status' => 'Pending',
+        ]);
     }
 }
